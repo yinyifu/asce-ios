4d015b94cc4860999ab662ca7fa0386246bbaa29
Baicheng <baicheng@buffalo.edu>
2017-12-02 16:14:44 -0500

    Loading tweets with hashtag or account name
    comment lines in ViewController.swift to switch from (searchQuery) and (Account)

---
+++ TwitterKitIOS/TimelinesObjC/TimelinesObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimelinesObjC
+//
+//  Created by Twitter on 5/26/15.
+//  Copyright (c) 2015 Twitter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TwitterKitIOS/TimelinesObjC/TimelinesObjC/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  TimelinesObjC
+//
+//  Created by Twitter on 5/26/15.
+//  Copyright (c) 2015 Twitter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <TwitterKit/TwitterKit.h>
+
+@interface ViewController : TWTRTimelineViewController
+
+
+@end
+
