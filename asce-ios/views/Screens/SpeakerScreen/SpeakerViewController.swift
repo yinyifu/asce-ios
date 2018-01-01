c907d26a5ce60359faa65479f9faf706c546dc70
Baicheng <baicheng@buffalo.edu>
2018-01-01 16:11:36 -0500

    intended to gather speaker name data from event page

--- asce-ios/views/Screens/SpeakerScreen/SpeakerViewController.swift
+++ asce-ios/views/Screens/SpeakerScreen/SpeakerViewController.swift
@@ -11,26 +11,31 @@ import UIKit
 
 class SpeakerViewController : UITableViewController
 {
-    var fruits = ["Apple2", "Apricot", "Banana", "Blueberry", "Cantaloupe", "Cherry", "Clementine", "Coconut", "Cranberry", "Fig", "Grape", "Grapefruit", "Kiwi fruit", "Lemon", "Lime", "Lychee", "Mandarine", "Mango", "Melon", "Nectarine", "Olive", "Orange", "Papaya", "Peach", "Pear", "Pineapple", "Raspberry", "Strawberry"]
-    
+    private var schedules = EventLoader.schedulee2!
+    var names: Array<String> = Array()
     
     
     override func viewDidLoad(){
+        for events in schedules{
+            for name in (events.speakers?.split(separator: ","))!{
+                names.append(String(name))
+            }
+        }
     }
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return fruits.count
+        return names.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SpeakerTableViewCell", for: indexPath) as! SpeakerTableViewCell
         
-        let fruitName = fruits[indexPath.row]
-        cell.nameLabel?.text = fruitName
-        cell.profileImage?.image = UIImage(named:fruitName)
+        let Name = names[indexPath.row]
+        cell.nameLabel?.text = Name
+        cell.profileImage?.image = UIImage(named:Name)
         return cell
     }
 }
