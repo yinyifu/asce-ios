6653c7bb3cb31bcf437e918c758377c9b08bebb5
Baicheng <baicheng@buffalo.edu>
2018-01-23 23:38:50 -0500

    updated all sponsor images

--- asce-ios/views/Screens/SponsorScreen/SponsorViewController.swift
+++ asce-ios/views/Screens/SponsorScreen/SponsorViewController.swift
@@ -11,24 +11,33 @@ import UIKit
 
 class SponsorViewController : UITableViewController
 {
-    var sponsorN = ["B&L","CPL"]
-    override func viewDidLoad() {
+    var titleN = ["Golden Sponsors","Silver Sponsors","Bronze Sponsors","ASCE"]
+    var sponsorN = [["CHA","CPL","PARSONS"],["ASCEREGION1","FA","TC","USI"],["B_L","EJP","SEAS"],["2LINES","20120830","20130802","20131223","20161104"]]
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
     }
     
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         return sponsorN.count
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sponsorN[section].count
+    }
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return titleN[section]
+    }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "SponsorTableViewCell", for: indexPath) as! SponsorTableViewCell
-        let name = sponsorN[indexPath.row]
-        cell.sponsorImage?.image = UIImage(named:name)
+        cell.sponsorImage?.image = UIImage(named:sponsorN[indexPath.section][indexPath.row])
         return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
